21876e58d4fa81cb6dd74284d81584b3242c41ae
Moniruzzaman Shanto <42086179+Shanto75@users.noreply.github.com>
2026-03-29 09:50:27 +0600

    add: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://mzshanto.epizy.com',
   integrations: [tailwind(), react()],
   output: 'static',
 });
